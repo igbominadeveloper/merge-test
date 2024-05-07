2c0e661b646883e70e7da1cacdee66b8945fb9de
Favour Afolayan <favourafolayan@gmail.com>
2024-05-07 13:00:19 +0100

    chore: stop npm publish

--- release.config.cjs
+++ release.config.cjs
@@ -3,4 +3,15 @@
  */
 module.exports = {
   branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    "@semantic-release/github",
+  ],
 };
