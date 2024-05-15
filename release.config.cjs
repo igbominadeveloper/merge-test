9ce3016a2bacdc3e8a288cf4b63b5f7503cb9d71
Favour Afolayan <favourafolayan@gmail.com>
2024-05-15 16:21:02 +0100

    ci: update release config (#208)

--- release.config.cjs
+++ release.config.cjs
@@ -12,23 +12,19 @@ module.exports = {
       },
     ],
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     [
       '@semantic-release/npm',
       {
         npmPublish: false,
       },
     ],
+    '@semantic-release/github',
     [
       '@semantic-release/git',
       {
-        assets: ['package.json'], // Specify files to commit
-        message: 'chore(release): update package.json version to ${nextRelease.version} [skip ci]', // Optional: commit message template
-      },
-    ],
-    [
-      '@semantic-release/github',
-      {
-        assets: ['.next'],
+        assets: ['package.json', 'CHANGELOG.md', 'yarn.lock'], // Specify files to commit
+        message: 'chore(release): version ${nextRelease.version} [skip ci]', // Optional: commit message template
       },
     ],
   ],
